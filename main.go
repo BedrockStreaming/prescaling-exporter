84186d51272979ecd09e25b78d89b6022d92c8ee
Jeremy PLANCKEEL <jplanckeel.externe@bedrockstreaming.com>
2022-08-04 10:03:06 +0200

    chiore: remove all contents not gh-pages

--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/bedrockstreaming/prescaling-exporter/cmd"
-)
-
-func main() {
-	cmd.Execute()
-}
--- pkg/apis/prescaling.bedrock.tech/v1/doc.go
+++
@@ -1,5 +0,0 @@
-// +k8s:deepcopy-gen=package
-// +k8s:defaulter-gen=TypeMeta
-// +groupName=prescaling.bedrock.tech
-
-package v1
